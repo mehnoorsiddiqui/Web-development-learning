65ea08c6c7ef294427d003f3ad95102207580caa
Mahnoor Siddiqui <57288492+mehnoorsiddiqui@users.noreply.github.com>
2021-05-30 17:03:30 +0500

    top & nav

--- Template converted/app.js
+++ Template converted/app.js
@@ -1,6 +1,9 @@
 const barslines = document.querySelector(".bars").children;
 const bars = document.querySelector(".bars");
 const navItems = document.querySelector(".navItems");
+const heading=document.querySelector(".heading");
+const backToTop = document.querySelector(".backToTop");
+
 //On click the nav items becomes visible
 bars.addEventListener("click", hideNavs);
 function hideNavs() {
@@ -9,6 +12,19 @@ function hideNavs() {
         navItems.classList.toggle("active");
     }
 }
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+}
+
 
 
 

474f472538ef3b822bc690183b0e23b18cce18e4
Mahnoor Siddiqui <57288492+mehnoorsiddiqui@users.noreply.github.com>
2021-09-28 16:30:41 +0500

    till ajax and JSON

---
+++ Asynchronous JavaScript ,ajax and Fetch API/ajax&JSON.js
@@ -0,0 +1,58 @@
+document.querySelector("#button1").addEventListener('click',loadCustomer);
+document.querySelector("#button2").addEventListener('click',loadCustomers);
+
+//Load Single Customer
+function loadCustomer(e){
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET','customer.json',true);
+
+    xhr.onload = function(){
+        if(this.status===200){
+            // console.log(this.responseText);
+            const customer  = JSON.parse(this.responseText);
+
+            const output = `
+            <ul>
+                <li>ID :${customer.id}</li>
+                <li>Name :${customer.name}</li>
+                <li>Company :${customer.company}</li>
+                <li>Phone :${customer.phone}</li>
+            </ul>
+            `;
+
+            document.querySelector('#customer').innerHTML= output;
+        }
+    }
+    xhr.send();
+}
+
+//Load Customers
+function loadCustomers(e){
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET','customers.json',true);
+
+    xhr.onload = function(){
+        if(this.status===200){
+            // console.log(this.responseText);
+            const customers  = JSON.parse(this.responseText);
+             
+            let output = '';
+            customers.forEach(function(){
+                output += `
+                <ul>
+                    <li>ID :${customer.id}</li>
+                    <li>Name :${customer.name}</li>
+                    <li>Company :${customer.company}</li>
+                    <li>Phone :${customer.phone}</li>
+                </ul>
+                `;
+            });
+          
+
+            document.querySelector('#customers').innerHTML= output;
+        }
+    }
+    xhr.send();
+}
